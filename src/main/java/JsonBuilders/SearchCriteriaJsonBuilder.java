a73de8a60905d05e60121ca92774ce17a1d752d1
kh11884 <kh11884@gmail.com>
2020-05-16 16:21:21 +0700

    Добавлен шаблон запроса поиска по наименованию товара и минимального количества покупок

--- src/main/java/JsonBuilders/SearchCriteriaJsonBuilder.java
+++ src/main/java/JsonBuilders/SearchCriteriaJsonBuilder.java
@@ -33,4 +33,36 @@ public class SearchCriteriaJsonBuilder {
         }
         return resultJsonObject;
     }
+
+    public JSONObject buildProductNameMinTimesCriteriaJSON(Connection connection, JSONObject productNameMinTimesCriteria) {
+        JSONObject resultJsonObject = new JSONObject();
+        try {
+            String productNameCriteria = productNameMinTimesCriteria.getString("productName");
+            int minTimesCriteria = productNameMinTimesCriteria.getInt("minTimes");
+
+            Statement stmt = connection.createStatement();
+            ResultSet resultSet = stmt.executeQuery("SELECT c.lastname, c.firstname, p.productname, COUNT(*)\n" +
+                    "FROM purchases pu\n" +
+                    "         LEFT JOIN customers c on pu.customer_id = c.id\n" +
+                    "         LEFT JOIN products p on pu.product_id = p.id\n" +
+                    "WHERE p.productname = '" + productNameCriteria + "'\n" +
+                    "GROUP BY c.lastname, c.firstname, p.productname\n" +
+                    "HAVING COUNT(*) >=+" + minTimesCriteria + ";");
+
+            resultJsonObject.put("criteria", productNameMinTimesCriteria);
+            resultJsonObject.put("results", new JSONArray());
+
+            while (resultSet.next()) {
+                resultJsonObject.append("results", new JSONObject()
+                        .put("lastName", resultSet.getString(1))
+                        .put("firstName", resultSet.getString(2)));
+            }
+
+            resultSet.close();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultJsonObject;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,15 +9,21 @@ public class Main {
         JSONObject lastNameCriteria = new JSONObject();
         lastNameCriteria.put("lastName", "Иванов");
 
+        JSONObject productNameMinTimesCriteria = new JSONObject();
+        productNameMinTimesCriteria.put("productName", "Капуста")
+                .put("minTimes", 4);
+
         DBConnector connection = new DBConnector();
         SearchCriteriaJsonBuilder searchCriteria = new SearchCriteriaJsonBuilder();
 
         JSONObject testResultJson = null;
+        JSONObject testResultJson2 = null;
 
         try {
             Connection con = connection.getDBConnecion();
             try {
                 testResultJson = searchCriteria.buildLastNameCriteriaJSON(con, lastNameCriteria);
+                testResultJson2 = searchCriteria.buildProductNameMinTimesCriteriaJSON(con, productNameMinTimesCriteria);
 
             } finally {
                 con.close();
@@ -26,5 +32,6 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println(testResultJson);
+        System.out.println(testResultJson2);
     }
 }
